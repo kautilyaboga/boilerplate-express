ffa95e273271fa9577472ee1376c495b4d19a42b
Kautilya Boga <kautilya@edwisely.com>
2023-02-28 02:51:55 +0530

    sendFile

--- myApp.js
+++ myApp.js
@@ -1,10 +1,13 @@
 let express = require('express');
 let app = express();
 
+absolutePath = __dirname + '/views/index.html'
+
 console.log("Hello World")
 
 app.get("/",function (req,res) {
     res.send("Hello Express")
+    res.sendFile(absolutePath)
 })
 
 
